bd6005b87ac8b652495b5cd3d9de561754ca9ebc
volsahin <cesahinvolkan@gmail.com>
2022-09-04 22:29:55 +0300

    Theme replaced with custom theme

--- app/src/main/java/com/tinyfalcon/dschallenge/MainActivity.kt
+++ app/src/main/java/com/tinyfalcon/dschallenge/MainActivity.kt
@@ -3,7 +3,12 @@ package com.tinyfalcon.dschallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.tinyfalcon.dschallenge.feature.home.presentation.MyTheme
 import com.tinyfalcon.dschallenge.feature.home.presentation.SessionsScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,8 +18,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                SessionsScreen()
+            MyTheme {
+                Box(modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black)
+                ) {
+                    SessionsScreen()
+                }
             }
         }
     }
