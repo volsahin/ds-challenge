e7e084d276a91e2f0b17f63542ca5b07038f0123
volsahin <cesahinvolkan@gmail.com>
2022-09-04 22:27:27 +0300

    Callbacks for page loading

--- app/src/main/java/com/tinyfalcon/dschallenge/feature/home/presentation/SessionMediator.kt
+++ app/src/main/java/com/tinyfalcon/dschallenge/feature/home/presentation/SessionMediator.kt
@@ -3,17 +3,22 @@ package com.tinyfalcon.dschallenge.feature.home.presentation
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.tinyfalcon.dschallenge.feature.home.data.PageLoadingState
+import com.tinyfalcon.dschallenge.feature.home.data.PageState
 import com.tinyfalcon.dschallenge.feature.home.domain.GetSessions
 import com.tinyfalcon.dschallenge.feature.home.domain.MusicSessionViewEntity
 import com.tinyfalcon.dschallenge.feature.home.domain.SearchSessions
 import com.tinyfalcon.dschallenge.feature.home.domain.SessionViewEntity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @ExperimentalPagingApi
 class SessionMediator @Inject constructor(
     private val currentPageState: PageState,
     private val getSessions: GetSessions,
-    private val searchSessions: SearchSessions
+    private val searchSessions: SearchSessions,
+    private val onPageLoad: (PageLoadingState) -> (Unit)
 ): PagingSource<Int, SessionViewEntity>() {
 
     companion object {
@@ -24,11 +29,17 @@ class SessionMediator @Inject constructor(
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SessionViewEntity> {
         val nextPage = params.key ?: FIRST_PAGE
 
+        onPageLoad(PageLoadingState.LOADING)
+
         val sessionViewEntity: MusicSessionViewEntity = if (currentPageState == PageState.LIST) {
             getSessions.execute()
         } else {
             searchSessions.execute()
         }
+
+        delay(600)
+
+        onPageLoad(PageLoadingState.READY)
 
         sessionViewEntity.data?.sessions?.let {
             return LoadResult.Page(
